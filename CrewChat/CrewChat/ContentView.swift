9b10da429469c14fa3eae0413b4da18f4006e1f6
Yashvardhan Arora <yash22arora@gmail.com>
2026-01-30 23:39:25 +0530

    Init XCode Project

---
+++ CrewChat/CrewChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrewChat
+//
+//  Created by Yashvardhan Arora on 30/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrewChat/CrewChat/CrewChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewChatApp.swift
+//  CrewChat
+//
+//  Created by Yashvardhan Arora on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CrewChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
